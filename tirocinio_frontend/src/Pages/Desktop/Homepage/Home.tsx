ebe10dcd430bd2bd0bd45e23d85c10cf697b1194
andreagrassi11 <andreagrassi200a@gmail.com>
2023-08-27 16:41:57 +0200

    Fix home

--- tirocinio_frontend/src/Pages/Desktop/Homepage/Home.tsx
+++ tirocinio_frontend/src/Pages/Desktop/Homepage/Home.tsx
@@ -1,10 +1,31 @@
-import { PieChart } from '@mui/x-charts/PieChart';
 import './Home.css';
 import { useEffect, useState } from 'react';
 import { checkJwt } from '../../../Utils/AuthService';
 import { useNavigate } from 'react-router-dom';
 import { TakeShow } from '../../../Utils/ShowService';
 import { TakeResultShow, TakeShowRealized, TakeUserShowId } from '../../../Utils/ResultService';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Pie } from 'react-chartjs-2';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+const options = {
+    plugins: {
+      legend: {
+        display: false // Hide the legend
+      }
+    }
+  };
+  
 
 function Home() {
 
@@ -14,6 +35,7 @@ function Home() {
     const [totPart, setTotPartecipanti] = useState<any>();
     const [totRes, setTotRes] = useState<any>();
     const [chartData, setChartData] = useState<any>([]);
+    const [labels, setLabels] = useState<any>([]);
 
     useEffect(() => {
         const check = async () => {
@@ -26,8 +48,18 @@ function Home() {
         }
 
         const takeShow = async () => {
-            let response = await TakeShowRealized();
-            setLists(response);
+            let count = 0;
+            let response:any = await TakeShowRealized();
+            
+            response.map((r:any, index:any) => {
+                count = count + r.showRealizeList.length;
+
+                r.showRealizeList.map((rr:any) => {
+                    labels.push(r.show.title + " - " + rr.place);
+                });
+            });
+            
+            setLists(count);
         }
 
         const takeUser = async () => {
@@ -36,25 +68,38 @@ function Home() {
             let totPartecipanti = 0;
             let totResult = 0;
             let chart:any = [];
+            let backgroundColor:any = [];
             
-            response.map((resp:any) => {
+            await Promise.all(response.map((resp:any) => {
                 resp.showRealizeList.map((showRelList:any) => {
                     idShowRel.push(showRelList.id);
                 });
-            });
+            }));
 
             await Promise.all(idShowRel.map(async (id:any, index:any) => {
                 let response:any = await TakeUserShowId(id);
-                totPartecipanti = response.length;
+                totPartecipanti = totPartecipanti + response.length;
 
                 let result:any = await TakeResultShow(id);
-                totResult = result.length; 
+                totResult = totResult + result.length; 
 
-                let rr = {'id': index,'value': response.length, 'label': 'Spettacolo'+index};
-                chart = [...chart, rr];
+                chart.push(result.length);
+                backgroundColor.push(getRandomColor());
             }));
 
-            setChartData([...chartData, chart])
+            let rr = {
+                labels: labels,
+                datasets: [
+                  {
+                    label: 'Voti',
+                    data: chart,
+                    backgroundColor: backgroundColor,
+                    borderWidth: 1,
+                  },
+                ],
+              };
+
+            setChartData(rr);
             setTotPartecipanti(totPartecipanti);
             setTotRes(totResult);
         }
@@ -76,7 +121,7 @@ function Home() {
                 <div className="w3-row bottomRowHome">
                     <div className="w3-col s6 sxHome">
                         <div className="my-center">
-                            <p style={{color:'white', fontSize:'25px', textAlign: 'center'}}>Totale spettacoli: {lists.length}</p>
+                            <p style={{color:'white', fontSize:'25px', textAlign: 'center'}}>Totale spettacoli: {lists}</p>
                             <p style={{color:'white', fontSize:'25px', textAlign: 'center'}}>Totale partecipanti: {totPart}</p>
                             <p style={{color:'white', fontSize:'25px', textAlign: 'center'}}>Risposte Ricevute: {totRes}</p>
                         </div>
@@ -85,16 +130,11 @@ function Home() {
                         <div className="my-center">
                             {
                                 chartData.length !== 0 ?
-                                (<PieChart
-                                    className='chart'
-                                    series={[
-                                        {
-                                        data: chartData[0],
-                                        },
-                                    ]}
-                                    width={500}
-                                    height={200}
-                                />)
+                                (
+                                    <div className='chart'>
+                                        <Pie data={chartData} options={options}/>
+                                    </div>
+                                )
                                 :
                                 ("")
                             }
